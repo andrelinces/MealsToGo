7511a927f745404c7b6d4d80c8dc13820e9b377f
andrelinces <andrelinces@yahoo.com.br>
2024-11-29 00:07:45 -0300

    feat Changed the presentation of the Restaurant Detail screen and added opacity when touching cards on the restaurants screen.

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,35 +1,42 @@
 import React, { useContext } from "react";
-import { StatusBar, SafeAreaView, FlatList, View } from 'react-native';
+import {
+  StatusBar,
+  SafeAreaView,
+  FlatList,
+  View,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
 import { RestaurantInfoCard } from "../components/restaurant-info-card.components";
 import styled from "styled-components";
 import { Spacer } from "../components/spacer/spacer.components";
 
 import { Search } from "../components/search.component";
 
-import { ActivityIndicator, MD2Colors } from 'react-native-paper';
+import { ActivityIndicator, MD2Colors } from "react-native-paper";
 
 import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
+import { Text } from "react-native-paper";
 const SafeArea = styled(SafeAreaView)`
-flex: 1;
-margin-top: ${StatusBar.currentHeight}px;
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
 `;
 
 const RestaurantListContainer = styled.View`
-flex: 1;
-padding: ${props => props.theme.space[3]};
-background-color: ${props => props.theme.colors.bg.primary};
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
 
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
     padding: 16,
-  }
-})
-``;
+  },
+})``;
 
 const Loading = styled(ActivityIndicator)`
-  marginLeft: -25px;
+  marginleft: -25px;
 `;
 
 const LoadingContainer = styled.View`
@@ -38,36 +45,37 @@ const LoadingContainer = styled.View`
   left: "50%";
 `;
 
+import { useNavigation } from "@react-navigation/native";
+
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
-  
+  const navigation22 = useNavigation();
+  console.log("navigation= ", navigation22.navigate);
+
   return (
     <SafeArea>
       {isLoading && (
         <LoadingContainer>
-          <Loading
-          size={50}
-          animating={true}
-          color={MD2Colors.blue300}
-          />
+          <Loading size={50} animating={true} color={MD2Colors.blue300} />
         </LoadingContainer>
       )}
-        <Search />
-        <RestaurantList
-          
-          data={restaurants}
-          
-          renderItem={({ item }) => {
-            // console.log("Item From RestaurantScreen =",item.name);
-           return (
-          
-          <Spacer position="bottom" size="large" >
-          <RestaurantInfoCard restaurant={item}/>
-          </Spacer>
-            );
-          }}
+      <Search />
+      <RestaurantList
+        data={restaurants}
+        renderItem={({ item }) => {
+          // console.log("Item From RestaurantScreen =",item.name);
+          return (
+            <TouchableOpacity
+              onPress={() => navigation22.navigate("RestaurantDetail")}
+            >
+              <Spacer position="bottom" size="large">
+                <RestaurantInfoCard restaurant={item} />
+              </Spacer>
+            </TouchableOpacity>
+          );
+        }}
         keyExtractor={(item) => item.name}
-        />
+      />
     </SafeArea>
-  )
+  );
 };
--- src/infrastructure/navigation/restaurants.navigator.js
+++ src/infrastructure/navigation/restaurants.navigator.js
@@ -1,18 +1,40 @@
 import React from "react";
 
+import { Text } from "react-native";
+
 import { RestaurantsScreen } from "../../features/restaurants/screens/restaurants.screen";
 
-import { createStackNavigator } from "@react-navigation/stack";
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from "@react-navigation/stack";
 
 const RestaurantStack = createStackNavigator();
 
+const RestaurantDetailScreen = () => {
+  return <Text>Restaurant Detail</Text>;
+};
+
 export const RestaurantsNavigator = () => {
   return (
-    <RestaurantStack.Navigator screenOptions={{ headerShown: false }}>
+    <RestaurantStack.Navigator>
       <RestaurantStack.Screen
-        name="Restaurants2"
+        name="Restaurants"
         component={RestaurantsScreen}
-        options={{ headerShown: false }} // Hide header for this screen only
+        options={{
+          //   presentation: "modal", // Enables the modal style
+          headerShown: false, // Optional: Hides the header for the modal
+          //   ...TransitionPresets.ModalPresentationIOS, // Ensures iOS-like modal transition
+        }}
+      />
+      <RestaurantStack.Screen
+        name="RestaurantDetail"
+        component={() => <Text Restaurant Detail></Text>}
+        options={{
+          //   presentation: "modal", // Enables the modal style
+          //   headerShown: false, // Optional: Hides the header for the modal
+          ...TransitionPresets.ModalPresentationIOS, // Ensures iOS-like modal transition
+        }}
       />
     </RestaurantStack.Navigator>
   );
